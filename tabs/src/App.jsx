4134f034c27f54dbb3d4bd2feec2cd13ad8e1c8f
Hana Pourmohebbi <h.pourmohebbi@gmail.com>
2024-08-24 23:16:09 +0430

    Use useState to create a state called activeTab with an empty initial value. setActiveTab is the function used to update the activeTab value.

--- tabs/src/App.jsx
+++ tabs/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
-import './index.css'
+import React, { useState, useEffect } from 'react';
+import './index.css';
+
 
 const tabs = [
   { id: 'tab1', title: 'Home', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' },
@@ -8,6 +9,6 @@ const tabs = [
   { id: 'tab4', title: 'Projects', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore' },
 ];
 function App(){
-
+  const [activeTab, setActiveTab] = useState('');
 }
 export default App
